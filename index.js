6aacbc9efd4911f6e476d4ed7296a2f28ce3913b
Ritesh672 <riteshyad672@gmail.com>
2024-02-29 14:12:25 +0530

    commit for the joke api page

--- index.js
+++ index.js
@@ -8,54 +8,41 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 8080;
 
-// the base url for the port
-
-var baseURL = "https://v2.jokeapi.dev/joke";
-
 // set the body parser
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public/"));
 
-// get request
+// i will build a website and it will have multiple apis intigreated in it
+// objective
+//step1: to create a home page which will have all the multiple apis in boxes and user can choose to any one of them
+//step2: for each api there would be multiple calls 
+
+// the get request for the home page
+
 app.get("/", (req, res)=>
 {
-    res.render("joke.ejs");
+  res.render("index.ejs");
 });
 
+// for the joke api
 
-app.post("/joke", async(req, res)=> {
-  try {
+app.post("/joke", async(req, res)=>
+{
+  try
+  {
     const response = await axios.get("https://v2.jokeapi.dev/joke/Any?type=single");
-    const joke = response.data.joke; // Access joke directly from response.data
-    console.log(joke);
-    res.render("joke.ejs", { joke }); // Pass joke directly to render
-  } catch (error) {
-    res.status(404).send(error.message);
+
+    const object = response.data;
+
+    console.log(object.joke);
+    res.render("joke.ejs", {data : object["joke"]});
   }
-});
-
-
-
-// for the custom jokes
-
-// app.post("/submit",  async(req, res)=>
-// {
-//     // create a function to make dynamic url for the axios
-
-//     const custom = req.body.custom;
-//     console.log(custom);
-
-//    try{
-//     const response = await axios.get(baseURL + `/${custom}?type=single`);
-//    }
-
-    
-
- 
-// });
-
-
+  catch(error)
+  {
+    res.status(404);
+  }
+})
 // listen the app on the server port
 
 app.listen(port, ()=>
